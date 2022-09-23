51da9638aebba26b22ebbf2456300adea60abb4b
PickleOnAString <69421001+PickleOnAString@users.noreply.github.com>
2022-09-24 02:14:00 +1000

    Added Files!!!

---
+++ obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SlimeModder2
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "SlimeModder2";
+        public const string PLUGIN_NAME = "SlimeModder2";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
